632e12d59662c04d86168e61cfdbfbfaab79daba
M Khairul Lukman W <rully@bippo.co.id>
2011-09-23 11:49:36 +0700

    sales_order.infoEx Web Service method

---
+++ Bippo/MagentoXtender/Model/Order/Api.php
@@ -0,0 +1,52 @@
+<?php
+class Bippo_MagentoXtender_Model_Order_Api extends Mage_Sales_Model_Order_Api
+{
+
+    /**
+     * Retrieve full order information
+     *
+     * @param string $orderIncrementId
+     * @return array
+     */
+    public function infoEx($orderIncrementId)
+    {
+        $order = $this->_initOrder($orderIncrementId);
+
+        if ($order->getGiftMessageId() > 0) {
+            $order->setGiftMessage(
+                Mage::getSingleton('giftmessage/message')->load($order->getGiftMessageId())->getMessage()
+            );
+        }
+
+        $result = $this->_getAttributes($order, 'order');
+
+        $result['shipping_address'] = $this->_getAttributes($order->getShippingAddress(), 'order_address');
+        $result['billing_address']  = $this->_getAttributes($order->getBillingAddress(), 'order_address');
+        $result['items'] = array();
+
+        foreach ($order->getAllItems() as $item) {
+        	if ($item->getGiftMessageId() > 0) {
+                $item->setGiftMessage(
+                    Mage::getSingleton('giftmessage/message')->load($item->getGiftMessageId())->getMessage()
+                );
+            }
+
+            $itemRow = $this->_getAttributes($item, 'order_item');
+            $itemRow['status_id'] = $item->getStatusId();
+            $itemRow['status'] = $item->getStatus();
+            $result['items'][] = $itemRow;
+
+        }
+
+        $result['payment'] = $this->_getAttributes($order->getPayment(), 'order_payment');
+
+        $result['status_history'] = array();
+
+        foreach ($order->getAllStatusHistory() as $history) {
+            $result['status_history'][] = $this->_getAttributes($history, 'order_status_history');
+        }
+
+        return $result;
+    }
+
+}
